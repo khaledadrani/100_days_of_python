91f787bd8da6fe162670653c37d9ccdfd7098b7c
Khaled Adrani <45245894+khaledadrani@users.noreply.github.com>
2021-10-04 13:48:03 +0200

    day_2 pushed

--- readme.md
+++ readme.md
@@ -1,2 +1,27 @@
 Following the course of this link. Trying to implement stuff on my own
 https://dev.to/iamdurga/day-1-of-100-days-of-code-python-basics-2e0m
+
+
+# day1: binary to decimal
+
+
+# day2: hexa decimal binary, 
+
+## issues
+https://stackoverflow.com/questions/1841565/valueerror-invalid-literal-for-int-with-base-10
+https://stackoverflow.com/questions/2951701/is-it-possible-to-use-else-in-a-list-comprehension
+https://www.permadi.com/tutorial/numDecToHex/
+https://owlcation.com/stem/How-to-Convert-Hex-to-Binary-and-Binary-to-Hexadecimal
+
+## epics
+first need to rework the code of functions
+
+* first regulate all converters!!!!
+* decimal binary hexa octal 
+* data structures (dict for memory and registers probably)
+*  processor? a class yes POO
+
+* re write code as a library!
+* https://stackoverflow.com/questions/4964975/is-it-possible-to-program-in-binary
+* http://ref.x86asm.net/
+* https://timelessname.com/elfbin/
---
+++ scripts/day_2_hexadecimal_to_binary.py
@@ -0,0 +1,82 @@
+from day_1_binary_to_decimal import binary_to_decimal, decimal_to_binary
+
+hex_dic = {
+    'A':10,
+    'B':11,
+    'C':12,
+    'D':13,
+    'E':14,
+    'F':15
+}
+
+
+def hex_to_decimal(nb):
+    nb = list(str(nb))[::-1]
+    ls = []
+    print(nb)
+    for i in range(len(nb)):
+        digit = nb[i]
+        if digit in hex_dic.keys():
+            digit = hex_dic[digit]
+        print(digit,type(digit))
+        ls.append(int(digit)*16**i)
+    return sum(ls)
+
+def decimal_to_hex(nb):
+    o = ""
+    inverted_hex_dic = {v:k for k,v in hex_dic.items()}
+    print(inverted_hex_dic)
+    while nb>0:
+        rest = nb % 16
+        nb = nb // 16
+        if rest in inverted_hex_dic.keys():
+            rest = inverted_hex_dic[rest]
+        o += str(rest)
+
+    return o[::-1]
+
+def hex_to_bin(nb):
+    ls = []
+    for n in list(nb)[::-1]:
+        n = hex_dic.get(n,n)
+        n = decimal_to_binary(n)
+        ls.append(n)
+    return "".join(ls)
+
+def bin_to_hex(nb):
+
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+    groups = list(chunks(list(nb)[::-1],4))
+
+    print('groups ',groups)
+
+    res = "".join([binary_to_decimal("".join(group)) for group in groups])
+
+    return decimal_to_hex(res)
+
+    
+hexa = "AAAA"
+decim = 43690
+
+correct_res = hexa
+
+print(decim)
+print(decimal_to_binary(decim))
+print(bin_to_hex("1010101010101010"))
+#print(correct_res)
+
+
+
+#first regulate all converters!!!!
+# decimal binary hexa octal 
+# data structures (dict for memory and registers probably)
+# processor? a class yes POO
+
+#re write code as a library!
+#https://stackoverflow.com/questions/4964975/is-it-possible-to-program-in-binary
+#http://ref.x86asm.net/
+#https://timelessname.com/elfbin/
