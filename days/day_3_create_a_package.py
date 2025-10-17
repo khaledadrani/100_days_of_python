b6b83a9e610accf6a339aadbabacf79351838e9e
khaledadrani <khaledadrani@gmail.com>
2025-10-17 10:02:26 +0100

    chore: remove days as it was moved to old_days

--- days/day_3_create_a_package.py
+++
@@ -1,7 +0,0 @@
-#I now know how to create a python package
-
-class PythonPackage(object):
-    def __init__(self,args):
-        self.init_file = args['init']
-        self.files = args['files']
-        self.internal_packages = [PythonPackage(p['arguments']) for p in args['internal_packages']]
